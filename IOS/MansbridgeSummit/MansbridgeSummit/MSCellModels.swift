03e632811ed7891462129b0ccb67259017802173
william-fiset <fisetwill@gmail.com>
2015-07-04 11:44:50 -0300

    Added Schedule json file

---
+++ IOS/MansbridgeSummit/MansbridgeSummit/MSCellModels.swift
@@ -0,0 +1,22 @@
+//
+//  MSCellModels.swift
+//  MansbridgeSummit
+//
+//  Created by William Fiset on 2015-07-04.
+//  Copyright © 2015 Mansbridge Summit Dev Team. All rights reserved.
+//
+
+import Foundation
+
+public class MSEvent {
+    
+    var eventTime : String = ""
+    var eventName : String = ""
+    var eventLocation : String = ""
+    
+    
+}
+
+public class MSCellDate {
+    
+}
