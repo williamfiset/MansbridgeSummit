ae5a619872ddf3b97e347309b53e755475a04d95
william-fiset <fisetwill@gmail.com>
2015-09-02 16:20:54 -0300

    Added layouts

---
+++ Android/MansbridgeSummit/app/src/main/java/com/app/micahstairs/mansbridgesummit/SpeakerActivity.java
@@ -0,0 +1,31 @@
+package com.app.micahstairs.mansbridgesummit;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by williamfiset on 15-09-02.
+ */
+public class SpeakerActivity extends ActionBarActivity {
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_speaker);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
